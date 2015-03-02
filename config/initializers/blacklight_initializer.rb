c54ad02a29b0101fbfe67c4bdadbe0ddd21470a8
Nathan Rogers <nrogers@clevelandart.org>
2015-03-02 15:30:05 -0500

    Removing a couple of configuration files from Github

--- config/initializers/blacklight_initializer.rb
+++
@@ -1,7 +0,0 @@
-# A secret token used to encrypt user_id's in the Bookmarks#export callback URL
-# functionality, for example in Refworks export of Bookmarks. In Rails 4, Blacklight
-# will use the application's secret key base instead.
-#
-
-# Blacklight.secret_key = 'aeb5b5209c4f077d3b6a9e92e20be606992dfb43359d19f59770bea21f107db9208e48956eb6c0b7d19c30baa021519d9bf3ae69a1cd91eadc3a50a53f322e1e'
-
