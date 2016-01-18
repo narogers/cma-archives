9895160efbe5b34a3e6479b1cd305e4ac27a7a5c
Nathan Rogers <nrogers@clevelandart.org>
2016-01-18 11:05:17 -0500

    Default collection description to the folder path

--- app/helpers/cma_helper.rb
+++ app/helpers/cma_helper.rb
@@ -8,4 +8,22 @@ module CMAHelper
 
     render partial, {member: member}
   end
+
+  # Given a collection object will choose either the description field,
+  # if present, or the path for the folder. This is a bit of a bandaid
+  # patch until the metadata can be updated
+  def description_for_collection collection
+     description = 'No description available'
+     if collection.description.present?
+       description = collection.description
+     else
+       first_member = collection.members.first
+       unless first_member.nil?
+         description = File.dirname(first_member.import_url)
+         description = Addressable::URI.parse(description).path
+       end
+     end
+
+     return description
+  end
 end
