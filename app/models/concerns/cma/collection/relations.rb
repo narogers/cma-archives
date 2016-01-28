b2b81b9fe938c5387898894298c7b569d473eb95
Nathan Rogers <nrogers@clevelandart.org>
2016-01-28 14:50:33 -0500

    Simplify collection relationships
    Since subcollections is not being utilized anymore take it out of the
    Collection model

--- app/models/concerns/cma/collection/relations.rb
+++ app/models/concerns/cma/collection/relations.rb
@@ -1,11 +1,12 @@
+# Adds similar functionality for collections that exists in GenericFiles
+# and their children. Specifically this acts as a stopgap until a migration
+# to PCDM which fully supports the notion of subcollections without any
+# extra work
 module CMA
   module Collection
     module Relations
       extend ActiveSupport::Concern
       included do
-        has_and_belongs_to_many :subcollections, 
-          predicate: ActiveFedora::RDF::Fcrepo::RelsExt.hasPart,
-          class_name: "ActiveFedora::Base"
         has_many :collections,
           predicate: ActiveFedora::RDF::Fcrepo::RelsExt.hasPart,
           class_name: "ActiveFedora::Base"
