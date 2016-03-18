227c97d45cc7826fa2e3c7024d48aaad9d7f0067
Nathan Rogers <nrogers@clevelandart.org>
2016-03-18 10:37:07 -0400

    Make batch creation tests pass

--- spec/jobs/batch_ingest_job_spec.rb
+++ spec/jobs/batch_ingest_job_spec.rb
@@ -1,14 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe BatchIngestJob do
-  describe "#run" do
-    before(:all) do
-      @parent = Collection.new(title: "Batch Tests")
-      @parent.edit_users = ["admin"]
-      @parent.depositor = "admin"
-      @parent.save
-    end
+  before(:each) do
+    @parent = Collection.new(title: "Batch Tests")
+    @parent.edit_users = ["admin"]
+    @parent.depositor = "admin"
+    @parent.save
+  end
 
+  describe "#run" do
     before(:each) do
       @parent.members = []
       @parent.save
@@ -20,12 +20,14 @@ RSpec.describe BatchIngestJob do
     end
 
     it "creates a new collection" do
-      #allow(ImportUrlJob).to receive(:new)
+      allow(ImportUrlJob).to receive(:new)
       allow(Sufia.queue).to receive(:push)
       job = BatchIngestJob.new "spec/fixtures/new_batch.csv"
       job.run
       
-      coll = Collection.where(title_tesim: "New Batch Ingest").first
+      # TODO: This suggests that Collection needs to be reworked
+      ids = Collection.find_with_conditions("title_tesim: \"New Batch Ingest\"")
+      coll = Collection.load_instance_from_solr(ids.first["id"])
       expect(coll.title).to eq "New Batch Ingest"
       expect(coll.date_created).to contain_exactly "2016-03"
       expect(coll.collections).to contain_exactly @parent
