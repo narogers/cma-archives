eb142e4ef8aacc6d0ac24692a2167827f87022f0
Nathan Rogers <nrogers@clevelandart.org>
2016-03-25 17:05:36 -0400

    Make tempfile accessible to characterization service

--- app/services/cma/characterization_service.rb
+++ app/services/cma/characterization_service.rb
@@ -6,10 +6,13 @@ module CMA
       return unless content.has_content?
 
       # Otherwise create a temporary version then run it against FITS
-      metadata = nil
       Hydra::Derivatives::TempfileService.create(content) do |f|
+        # Make the file world readable so the service can actually see.
+        # Otherwise you get some weird errors from FITS
+        f.chmod(0644)
         response = HTTParty.get(uri, query: {file: f.path})
         if 200 == response.code
+          binding.pry
           return response.body
         else
           raise UnexpectedServerResponse("Received HTTP status code #{response.code} from #{uri}")
