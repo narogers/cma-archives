1a1b4f4763e2f7f6df8e5685533dc83056e97e4b
Nathan Rogers <nrogers@clevelandart.org>
2016-03-30 11:11:26 -0400

    Retry Net::Timeout before throwing an error

--- app/services/cma/characterization_service.rb
+++ app/services/cma/characterization_service.rb
@@ -1,7 +1,10 @@
 module CMA
   class CharacterizationService
     def self.characterize content
+      # TODO: Make these configurable
       uri = "http://127.0.0.1:8888/fits/FitsService"
+      max_retries = 3
+
       # No point continuing if there is nothing to process
       return unless content.has_content?
 
@@ -10,7 +13,20 @@ module CMA
         # Make the file world readable so the service can actually see.
         # Otherwise you get some weird errors from FITS
         f.chmod(0644)
-        response = HTTParty.get(uri, query: {file: f.path})
+
+        times_retried = 0
+        begin
+          response = HTTParty.get(uri, query: {file: f.path}, timeout: 180)
+        rescue Net::Timeout
+          if times_retried < max_retries
+            times_retried += 1
+            Rails.logger.warn "[CHARACTERIZE] Unable to connect to uri"
+            retry
+          else
+            Rails.logger.warn "[CHARACTERIZE] Could not connect to #{uri} after {#max_retries} attempts"
+            raise UnexpectedServerResponse("Connection repeatedly timed out") 
+          end 
+        end
         if 200 == response.code
           #binding.pry
           # Kludge because the information that comes back from FITS Servlet is not
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -55,7 +55,7 @@ Rails.application.configure do
   # config.logger = ActiveSupport::TaggedLogging.new(SyslogLogger.new)
 
   # Use a different cache store in production.
-  # config.cache_store = :mem_cache_store
+  config.cache_store = :mem_cache_store
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = 'http://assets.example.com'
@@ -79,7 +79,9 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  # Silence deprecation warnings in production only
+  # Reduce amount of information in the logs for production to reduce
+  # signal to noise ratio
   ActiveSupport::Deprecation.silenced = true
   Deprecation.default_deprecation_behavior = :silence
+  ActiveFedora::Base.logger.level = :warn if ActiveFedora::Base.logger
 end
