c6ce3c8e803653e4f3c18c3e1fbcda00a8e950e2
Nathan Rogers <nrogers@clevelandart.org>
2016-04-11 10:20:10 -0400

    Use system logrotate to automatically rotate production logs

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -73,8 +73,7 @@ Rails.application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
-  config.logger = Logger.new(Rails.root.join("log", 
-    Rails.env + ".log"), 'daily')
+  config.logger = Logger.new(Rails.root.join("log", Rails.env + ".log"))
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
