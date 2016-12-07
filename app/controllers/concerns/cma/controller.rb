a8f0ce05d8a76f63d9d96c57f9f4e588931356b6
Nathan Rogers <nrogers@clevelandart.org>
2016-12-07 12:03:07 -0500

    Redirect to the correct page after log in

--- app/controllers/concerns/cma/controller.rb
+++ app/controllers/concerns/cma/controller.rb
@@ -1,16 +1,8 @@
 # Overrides for Blacklight, Sufia, and general HYdra controller behaviours
 module CMA
   module Controller
-    # Crib this from Devise examples in the Platformatec wiki
-    def after_sign_in_path_for(_resource)
-      sign_in_url = new_user_session_url
-      Rails.logger.info "[SIGN IN] #{sign_in_url}"
-
-      if request.referer == sign_in_url
-        root_path
-      else
-        request.referer || root_path
-      end
+    def after_sign_in_path_for(resource)
+      stored_location_for(resource) || root_path
     end
   end  
 end
